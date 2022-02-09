14ec52816251297eb7ff86027c15909373b77b57
muboluo <muboluo@126.com>
2022-02-10 00:56:26 +0800

    first commit

---
+++ app/src/main/java/app/sample/retrofit/MainActivity.java
@@ -0,0 +1,16 @@
+package app.sample.retrofit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// 练习 retrofit 注解部分
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
